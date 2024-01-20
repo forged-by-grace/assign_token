7125a86f65d7676aa73172ed5f34493e03a9c315
James Nornubari <nornubariconfidence@gmail.com>
2024-01-20 17:18:02 +0100

    Updated requirements and main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from core.event.consume_event import consume_assign_token_event, consume_reused_refresh_token_event, consume_revoke_token_event, consume_update_token_event
+from core.event.consume_event import consume_assign_token_event
 import asyncio
 from collections import Counter
 
 async def main():
     await asyncio.gather(
-        consume_assign_token_event(), 
-        consume_reused_refresh_token_event(), 
-        consume_revoke_token_event(), 
-        consume_update_token_event(),
+        consume_assign_token_event(),         
     )
 
 asyncio.run(main=main())
